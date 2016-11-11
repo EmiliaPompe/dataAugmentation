7f49548597026a0a365b5d6829309246ee259794
EmiliaPompe <pompe.emilia@gmail.com>
2016-11-11 11:17:21 +0000

    PX DA method added to t-student example

--- t_student_example.R
+++ t_student_example.R
@@ -1,5 +1,4 @@
-######trial####
-
+# standard DA for a toy example - simulating from the t-student distribution
 # N is the number of iterations; x_0 - starting value for x
 N <- 100000
 x_0 <- 1
@@ -29,3 +28,32 @@ hist(x[burn_in:N], 100, col='yellow', las=1, border=T, freq=FALSE, xlim=c(c(-30,
 hist(rt(N-burn_in, df=4), 100, col='red', las=1, border=F, freq=FALSE,  xlim=c(-30, 30), add=TRUE)
 
 # they are very similar
+
+# PXDA for our toy example (page 29 in the Chapter)
+# apart from N and x_0 we need to set alpha and beta
+N <- 100000
+x_0 <- 1
+burn_in <- 1000
+alpha <- 1
+beta <- 1
+
+x <- rep(0, N+1)
+y <- rep(0, N)
+x[1] <- x_0
+
+set.seed(13)
+
+for (i in seq_len(N)){
+  # drawing Y given X
+  u <- rf(1, 5, 2*alpha) #  f distribution
+  y[i] <- u * 10*beta/(alpha*(x[i]^2+4))
+  
+  # drawing X given Y
+  v <- rt(1, 2*alpha+4) #  t distribution
+  x[i+1] <- v * sqrt(2*(y[i]+beta)/(y[i]*(alpha+2)))
+}
+
+hist(x[burn_in:N], 100, col='yellow', las=1, border=T, freq=FALSE, xlim=c(c(-30, 30)))
+# comparing with the t distribution with 4 degrees of freedom
+hist(rt(N-burn_in, df=4), 100, col='red', las=1, border=F, freq=FALSE,  xlim=c(-30, 30), add=TRUE)
+
